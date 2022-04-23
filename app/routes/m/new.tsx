0824b20dd09b605163b78b24c82e9fe705e9f7b2
Bruno Quaresma <brunoquaresma@Brunos-MacBook-Pro.local>
2022-04-23 11:23:42 -0300

    style: fix mobile design

--- app/routes/m/new.tsx
+++ app/routes/m/new.tsx
@@ -7,6 +7,7 @@ import {
   IconButton,
   Icon,
   useClipboard,
+  Link,
 } from "@chakra-ui/react";
 import { ActionFunction, json, redirect } from "@remix-run/node";
 import { useSearchParams } from "@remix-run/react";
@@ -40,29 +41,57 @@ export default function NewMessage() {
     <Container maxW="container.xl" px={4} py={20}>
       <Center>
         <Box textAlign="center">
-          <Text fontSize="xl" display="block" fontWeight="bold">
+          <Text fontSize="2xl" display="block" fontWeight="bold">
             The link for your message is
           </Text>
-          <HStack spacing={3} borderWidth={1} p={2} pl={3} mt={2}>
-            <Text
+
+          <HStack
+            spacing={0}
+            borderWidth={1}
+            mt={4}
+            maxW={{ base: 360, md: "100%" }}
+          >
+            <Box
+              py={2}
+              pl={3}
               display="block"
               letterSpacing="wider"
               userSelect="all"
               color="gray.700"
+              whiteSpace="nowrap"
+              overflowX="auto"
             >
               https://onetimemessage.com/m/
               <span data-testid="messageId">{id}</span>
-            </Text>
-            <IconButton
-              disabled={copy.hasCopied}
-              onClick={copy.onCopy}
-              size="sm"
-              icon={
-                copy.hasCopied ? <Icon as={FiCheck} /> : <Icon as={FiCopy} />
-              }
-              aria-label="Copy message url"
-            />
+            </Box>
+            <Box p={2}>
+              <IconButton
+                disabled={copy.hasCopied}
+                onClick={copy.onCopy}
+                size="sm"
+                icon={
+                  copy.hasCopied ? <Icon as={FiCheck} /> : <Icon as={FiCopy} />
+                }
+                aria-label="Copy message url"
+              />
+            </Box>
           </HStack>
+
+          <Box mt={4}>
+            <Link fontWeight="medium" href="/">
+              Create a new message
+            </Link>
+          </Box>
+
+          <Box as="footer" w="full" fontSize="sm" color="gray.600" mt={20} py={4}>
+              Made by{" "}
+              <Link
+                href="https://twitter.com/bruno__quaresma"
+                fontWeight="bold"
+              >
+                @bruno__quaresma
+              </Link>
+            </Box>
         </Box>
       </Center>
     </Container>
